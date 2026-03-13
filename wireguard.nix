4d757eb8b1e273bb4a505b6ccc98abb2dc45dc88
ranger097 <78629373+ranger097@users.noreply.github.com>
2026-03-13 04:28:38 -0400

    updated configs

--- wireguard.nix
+++ wireguard.nix
@@ -1,42 +1,22 @@
 { config, pkgs, ... }: {
-networking.firewall = {
-    allowedUDPPorts = [ 51820 ]; # Clients and peers can use the same port, see listenport
-  };
-  # Enable WireGuard
-  networking.wireguard.interfaces = {
-    # "wg0" is the network interface name. You can name the interface arbitrarily.
-    wg0 = {
-      # Determines the IP address and subnet of the client's end of the tunnel interface.
-      ips = [ "10.100.0.2/24" ];
-      listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
+  networking.firewall.allowedUDPPorts = [ 51820 ];
 
-      # Path to the private key file.
-      #
-      # Note: The private key can also be included inline via the privateKey option,
-      # but this makes the private key world-readable; thus, using privateKeyFile is
-      # recommended.
+  networking.wg-quick.interfaces = {
+    wg0 = {
+      # Use 'address' instead of 'ips' for wg-quick
+      address = [ "10.100.0.2/24" ];
+      listenPort = 51820;
+
       privateKeyFile = "${config.users.users.ranger.home}/wireguard-keys/private";
 
       peers = [
-        # For a client configuration, one peer entry for the server will suffice.
-
         {
-          # Public key of the server (not a file path).
           publicKey = "Ad6HapBOd+2yCE6wSTfIC436VXrUZkhpmVaWtjEuwkQ=";
-
-          # Forward all the traffic via VPN.
           allowedIPs = [ "0.0.0.0/0" ];
-          # Or forward only particular subnets
-          #allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
-
-          # Set this to the server IP and port.
-          endpoint = "[2a0d:5600:24:48::10]:51820"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
-
-          # Send keepalives every 25 seconds. Important to keep NAT tables alive.
+          endpoint = "[2a0d:5600:24:48::10]:51820";
           persistentKeepalive = 25;
         }
       ];
     };
   };
-
 }
